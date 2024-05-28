689a16e93c2b7eb477e1d540d81a3ae612b366b4
Andrii Kyrychenko <akyrych84@gmail.com>
2024-05-28 12:41:20 -0300

    Finished work with followers. Now you see the messages of only those who are subscribed to.

---
+++ UICoordinator/Core/Activity/ViewModel/ActivityViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ActivityViewModel.swift
+//  UICoordinator
+//
+//  Created by Andrii Kyrychenko on 26/05/2024.
+//
+
+import Foundation
---
+++ UICoordinator/Services/Activity/ActivityService.swift
@@ -0,0 +1,8 @@
+//
+//  ActivityService.swift
+//  UICoordinator
+//
+//  Created by Andrii Kyrychenko on 01/05/2024.
+//
+
+import Foundation
