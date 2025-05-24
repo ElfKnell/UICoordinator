e591acf9723ec09391c7567af8721df21dc55326
Andrii Kyrychenko <akyrych84@gmail.com>
2025-05-24 13:42:03 +0200

    Added license information from Wikipedia.

---
+++ UICoordinator/Utils/MapInformation/View/WikipediaAttributionView.swift
@@ -0,0 +1,18 @@
+//
+//  WikipediaAttributionView.swift
+//  UICoordinator
+//
+//  Created by Andrii Kyrychenko on 24/05/2025.
+//
+
+import SwiftUI
+
+struct WikipediaAttributionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WikipediaAttributionView()
+}
